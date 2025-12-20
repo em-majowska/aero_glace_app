43d3e93c0c9622e1d9f295d2abedb06ae5616b66
Ewa Majowska <em.majowska@gmail.com>
2025-12-20 20:10:29 +0400

    Add glossy template

---
+++ lib/util/glossy_box.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+
+class GlossyBox extends StatelessWidget {
+  final Widget child;
+
+  const GlossyBox({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 0, top: 0, right: 0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: GradientBoxBorder(
+                gradient: LinearGradient(
+                  colors: [
+                    const Color(0xFFffffff).withValues(alpha: 0.95),
+                    const Color(0xFFFFFFFF).withValues(alpha: 0.75),
+                  ],
+                ),
+              ),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  const Color(0xFFffffff).withValues(alpha: 0.75),
+                  const Color(0xFFFFFFFF).withValues(alpha: 0.25),
+                ],
+                stops: [
+                  0.1,
+                  1,
+                ],
+              ),
+            ),
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
